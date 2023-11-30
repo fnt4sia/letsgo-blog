3d3afda2384b694148c657466cd1824642d95e71
fnt4sia <fitraramadhan668@gmail.com>
2023-12-01 02:43:39 +0700

    yea

--- src/pages/listDestinationPage.js
+++ src/pages/listDestinationPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../style/index.css';
 import Navbar from "../components/navbar";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import {Link} from 'react-router-dom';
 
 export default function ListDestinationPage(){
     let count = 1; //:D
     const sliderRef = useRef(null);
+    const[destinationData, setDestinationData] = useState([]);
 
     useEffect(() => {
         const slider = sliderRef.current;
@@ -20,11 +21,25 @@ export default function ListDestinationPage(){
             count++;
         }, 3000);
 
-        return () => clearInterval(interval); // Clean up on component unmount
+        return () => clearInterval(interval);
     }, []);
+
+    useEffect(() => {
+        fetch('https://letsgo-blog-default-rtdb.asia-southeast1.firebasedatabase.app/destination.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const destinationArray = Object.keys(data).map((key) => ({
+                id: key,
+                ...data[key]
+            }));
+            setDestinationData(destinationArray);
+        })
+    }, [])
+
+
     return(
         <>
-        <Navbar/><Navbar/>
+            <Navbar/><Navbar/>
             <div class="h-72 mt-10  bg-black relative grid place-items-center overflow-x-hidden -z-10">
                 <div id="slider" class="slider absolute h-full w-full flex ease-in-out" ref={sliderRef}>
                     <img src="https://blog-images.reddoorz.com/uploads/image/file/4511/prambanan-2010-2-of-2.jpg" class="h-full min-w-full object-cover opacity-50 "></img>
@@ -44,66 +59,23 @@ export default function ListDestinationPage(){
 
 
             <div class="flex mt-3 flex-wrap gap-5 p-3 md:px-24 md:gap-12 md:mt-8">
-                <div class="bg-gray-100 w-40 md:w-44 rounded-md border-gray-300 border-2 flex flex-col justify-between gap-2 hover:scale-105 ease-in-out duration-150">
-                    <img src="images/Candi-Prambanan-3.jpg" class="w-full rounded-md h-48 object-cover"></img>
-                    <hr class="mt-2"></hr>
-                    <h3 class="text-center font-bold">Prambanan</h3>
-                    <hr></hr>
-                    <div class="tags flex overflow-x-auto gap-1 p-1">
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Wisata kota</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Candi</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Peninggalan bersejarah</p>
-                    </div>
-                </div>
-
-                <div class="bg-gray-100 w-40 md:w-44 rounded-md border-gray-300 border-2 flex flex-col justify-between gap-2 hover:scale-105 ease-in-out duration-150">
-                    <img src="images/Candi-Prambanan-3.jpg" class="w-full rounded-md h-48 object-cover"></img>
-                    <hr class="mt-2"></hr>
-                    <h3 class="text-center font-bold">Candi ujung dunia</h3>
-                    <hr></hr>
-                    <div class="tags flex overflow-x-auto gap-1 p-1">
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Wisata kota</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Candi</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Peninggalan bersejarah</p>
-                    </div>
-                </div>
-                
-                <div class="bg-gray-100 w-40 md:w-44 rounded-md border-gray-300 border-2 flex flex-col justify-between gap-2 hover:scale-105 ease-in-out duration-150">
-                    <img src="images/Candi-Prambanan-3.jpg" class="w-full rounded-md h-48 object-cover"></img>
-                    <hr class="mt-2"></hr>
-                    <h3 class="text-center font-bold">Lorem, ipsum dolor sit amet consectetur</h3>
-                    <hr></hr>
-                    <div class="tags flex overflow-x-auto gap-1 p-1">
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Wisata kota</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Candi</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Peninggalan bersejarah</p>
-                    </div>
-                </div>
-                
-                <div class="bg-gray-100 w-40 md:w-44 rounded-md border-gray-300 border-2 flex flex-col justify-between gap-2 hover:scale-105 ease-in-out duration-150">
-                    <img src="images/Candi-Prambanan-3.jpg" class="w-full rounded-md h-48 object-cover"></img>
-                    <hr class="mt-2"></hr>
-                    <h3 class="text-center font-bold">Lorem, ipsum dolor sit amet consectetur</h3>
-                    <hr></hr>
-                    <div class="tags flex overflow-x-auto gap-1 p-1">
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Wisata kota</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Candi</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Peninggalan bersejarah</p>
-                    </div>
-                </div>
-                
-                <div class="bg-gray-100 w-40 md:w-44 rounded-md border-gray-300 border-2 flex flex-col justify-between gap-2 hover:scale-105 ease-in-out duration-150">
-                    <img src="images/Candi-Prambanan-3.jpg" class="w-full rounded-md h-48 object-cover"></img>
-                    <hr class="mt-2"></hr>
-                    <h3 class="text-center font-bold">Lorem, ipsum dolor sit amet consectetur</h3>
-                    <hr></hr>
-                    <div class="tags flex overflow-x-auto gap-1 p-1">
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Wisata kota</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Candi</p>
-                        <p class="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">Peninggalan bersejarah</p>
-                    </div>
-                </div>
-                
+                {
+                    destinationData && destinationData.reverse().map((item) => (
+                        <div class="bg-gray-100 w-40 md:w-44 rounded-md border-gray-300 border-2 flex flex-col justify-between gap-2 hover:scale-105 ease-in-out duration-150">
+                            <img src={item.image} class="w-full rounded-md h-48 object-cover"></img>
+                            <hr class="mt-2"></hr>
+                            <h3 class="text-center font-bold">{item.title}</h3>
+                            <hr></hr>
+                            <div class="tags flex overflow-x-auto gap-1 p-1">
+                                {
+                                    item.tag && item.tag.map((tag) => (
+                                        <p className="whitespace-nowrap p-0.5 px-2 bg-gray-200 rounded-lg text-sm">{tag}</p>
+                                    ))
+                                }
+                            </div>
+                        </div>                
+                    ))
+                }
             </div>
         </>
     );
