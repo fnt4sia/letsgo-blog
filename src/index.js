890db2910f3e0bb7f0c10a894c329b83a74d28fe
fnt4sia <fitraramadhan668@gmail.com>
2023-11-30 21:29:38 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import ListDestinationPage from './pages/listDestinationPage';
 import BlogPage from './pages/blogPage';
 import SearchPage from './pages/searchPage';
 import LogOut from './services/logout';
-import AddeventPage from './pages/addEvent';
+import AddeventPage from './pages/addEventPage';
 
 import {
   createBrowserRouter,
