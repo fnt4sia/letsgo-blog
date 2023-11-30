21bbcb68a83d57dcbd21dbd412bba961c683accc
fnt4sia <fitraramadhan668@gmail.com>
2023-12-01 05:28:29 +0700

    logout

--- src/pages/accountPage.js
+++ src/pages/accountPage.js
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { VscAccount } from "react-icons/vsc";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link} from "react-router-dom";
 
 export default function AccountPage(){
 
@@ -59,7 +59,13 @@ export default function AccountPage(){
                         </div>
                         <div className="flex flex-col">
                             <h3 className="text-lg font-bold">{name}</h3>
-                            <button className="bg-gray-200 w-fit p-0.5 py-2 px-4 rounded-md mt-1">logout</button>
+                            {
+                                localStorage.getItem('username') === name ? (
+                                    <Link to='/logout'><button className="bg-gray-200 w-fit p-0.5 py-2 px-4 rounded-md mt-1">logout</button></Link>
+                                ) : (
+                                    <button className="bg-gray-200 w-fit p-0.5 py-2 px-4 rounded-md mt-1">follow</button>
+                                )
+                            }
                         </div>
                     </div>
     
